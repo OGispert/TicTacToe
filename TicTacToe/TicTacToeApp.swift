278a3e8c06bdb0599414dc0780432f1600eb0204
Othmar Gispert <ogisq@allstate.com>
2022-10-06 17:39:42 -0500

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by ogisq on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
